1e2e6d2c7652be9e6fba87be189e6743ff251989
ygorsm08 <ygor.sm08@gmail.com>
2026-08-06 20:07:04 -0300

    Adicionar arquivos de projeto.

---
+++ MauiAppMinhasCompras/Models/Produto.cs
@@ -0,0 +1,13 @@
+﻿using SQLite;
+
+namespace MauiAppMinhasCompras.Models
+{
+    public class Produto
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        public string Descricao { get; set; }
+        public double Quantidade { get; set; }
+        public double Preco { get; set; }
+    }
+}
