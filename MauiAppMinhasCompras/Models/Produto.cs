4e0fd5602bf83cf5eb3566ae56b262a25a360259
ygorsm08 <ygor.sm08@gmail.com>
2026-09-03 21:24:45 -0300

    agenda 5

--- MauiAppMinhasCompras/Models/Produto.cs
+++ MauiAppMinhasCompras/Models/Produto.cs
@@ -4,9 +4,20 @@ namespace MauiAppMinhasCompras.Models
 {
     public class Produto
     {
+        string _descricao; 
+
         [PrimaryKey, AutoIncrement]
         public int Id { get; set; }
-        public string Descricao { get; set; }
+        public string Descricao {
+            get => _descricao;
+            set
+            {
+                if(value == null)
+                {
+                    throw new Exception("Por favor, preencha a descrição");
+                }
+            }
+        }
         public double Quantidade { get; set; }
         public double Preco { get; set; }
         public double Total { get => Quantidade * Preco; }
--- MauiAppMinhasCompras/Views/ListaProduto.xaml.cs
+++ MauiAppMinhasCompras/Views/ListaProduto.xaml.cs
@@ -16,9 +16,18 @@ public partial class ListaProduto : ContentPage
 
 	protected async override void OnAppearing()
 	{
-		List<Produto> tmp = await App.Db.GetAll();
+		try
+		{
+			lista.Clear();
+			List<Produto> tmp = await App.Db.GetAll();
 
-		tmp.ForEach(i => lista.Add(i));
+			tmp.ForEach(i => lista.Add(i));
+		}
+		catch (Exception ex)
+		{
+			await DisplayAlert("Ops", ex.Message, "OK");
+		}
+
 	}
 
 	private void ToolbarItem_Clicked(object sender, EventArgs e) 
@@ -35,13 +44,20 @@ public partial class ListaProduto : ContentPage
 
 	private async void txt_search_TextChanged(object sender, TextChangedEventArgs e)
 	{
-		string q = e.NewTextValue;
+		try
+		{
+			string q = e.NewTextValue;
 
-		lista.Clear();
+			lista.Clear();
 
-        List<Produto> tmp = await App.Db.Search(q);
+			List<Produto> tmp = await App.Db.Search(q);
 
-        tmp.ForEach(i => lista.Add(i));
+			tmp.ForEach(i => lista.Add(i));
+		}
+		catch (Exception ex)
+		{
+			await DisplayAlert("Ops", ex.Message, "OK");
+		}
     }
 
 	private void ToolbarItem_Clicked_1(object sender, EventArgs e)
@@ -53,8 +69,43 @@ public partial class ListaProduto : ContentPage
 		DisplayAlert("Total dos Produtos", msg, "OK");
 	}
 
-    private void MenuItem_Clicked(object sender, EventArgs e)
+    private async Task MenuItem_Clicked(object sender, EventArgs e)
     {
+		try
+		{
+			MenuItem selecionado = sender as MenuItem;
 
+			Produto p = selecionado.BindingContext as Produto;
+
+			bool confirm = await DisplayAlert(
+				"Tem Certeza?", $"Remover {p.Descricao}?", "Sim", "Não");
+
+			if(confirm)
+			{
+				await App.Db.Delete(p.Id);
+				lista.Remove(p);
+			}
+		}
+		catch (Exception ex)
+		{
+			await DisplayAlert("Ops", ex.Message, "OK");
+		}
+    }
+
+    private void lst_produtos_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+    {
+		try
+		{
+			Produto p = e.SelectedItem as Produto;
+
+			Navigation.PushAsync(new Views.EditarProduto
+			{
+				BindingContext = p,
+			});
+		}
+		catch (Exception ex)
+        {
+            DisplayAlert("Ops", ex.Message, "OK");
+        }
     }
 }
